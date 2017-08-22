ed1cbf4eb0cf9ce86172636e2813b0fdf6d465a2
Peter Kim <peterkim@Peters-MacBook-Pro.local>
2017-08-22 10:51:54 -0700

    Initial Commit

---
+++ PracticeGame2Tests/PracticeGame2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PracticeGame2Tests.swift
+//  PracticeGame2Tests
+//
+//  Created by Peter Kim on 8/22/17.
+//  Copyright © 2017 Peter Kim. All rights reserved.
+//
+
+import XCTest
+@testable import PracticeGame2
+
+class PracticeGame2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
